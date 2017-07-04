a34d352dcc59f63213a2ae3a30c9eb0462468ba9
Haozhe Gu  (ASH) <ashghz26@gmail.com>
2017-07-04 12:52:26 -0700

    Create [454] [Four Sum II] [Easy] [Binary Search Hash Table] [] [18].cpp

---
+++ [454] [Four Sum II] [Easy] [Binary Search Hash Table] [] [18].cpp
@@ -0,0 +1,27 @@
+//Question# + Difficulty + Topic + Company + Similar_Question
+//[454] [Four Sum II] [Easy] [Binary Search Hash Table] 
+//[] 
+//[18].cpp
+
+/*Four Sum*/
+class Solution {
+public:
+    int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+        unordered_map<int, int>  abSum;
+        for(auto a : A) {
+            for(auto b : B) {
+                ++abSum[a+b];
+            }
+        }
+        int count = 0;
+        for(auto c : C) {
+            for(auto d : D) {
+                auto it = abSum.find(0 - c - d);
+                if(it != abSum.end()) {
+                    count += it->second;
+                }
+            }
+        }
+        return count;
+    }
+};
