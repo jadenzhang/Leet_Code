0f642a73c3c0821f75aa9df0b85b9c9730ce80bf
Haozhe Gu  (ASH) <ashghz26@gmail.com>
2017-07-11 00:25:23 -0700

    Create [405] [Convert a Number to Hexadecimal] [Easy] [Bit Manipulation ]  []  [].cpp

---
+++ [405] [Convert a Number to Hexadecimal] [Easy] [Bit Manipulation ]  []  [].cpp
@@ -0,0 +1,21 @@
+/*
+Question# + Difficulty + Topic + Company + Similar_Question
+[405] [Convert a Number to Hexadecimal] [Easy] [Bit Manipulation ] 
+[] 
+[].cpp
+*/
+
+const string HEX = "0123456789abcdef";
+class Solution {
+public:
+    string toHex(int num) {
+        if (num == 0) return "0";
+        string result;
+        int count = 0;
+        while (num && count++ < 8) {
+            result = HEX[(num & 0xf)] + result;
+            num >>= 4;
+        }
+        return result;
+    }
+};
