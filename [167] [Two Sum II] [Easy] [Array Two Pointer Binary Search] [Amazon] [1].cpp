935cde2943581938307f77d74ec3aae72ea7eb14
Haozhe Gu  (ASH) <ashghz26@gmail.com>
2017-07-14 22:05:57 -0700

    Update [167] [Two Sum II] [Easy] [Array Two Pointer Binary Search] [Amazon] [1].cpp

--- [167] [Two Sum II] [Easy] [Array Two Pointer Binary Search] [Amazon] [1].cpp
+++ [167] [Two Sum II] [Easy] [Array Two Pointer Binary Search] [Amazon] [1].cpp
@@ -3,3 +3,19 @@
 //[Amazon] 
 //[1].cpp
 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int front = 0;
+        int back  = numbers.size()-1;
+        while(front<back)
+        {
+            int diff = target - numbers[front] - numbers[back];
+            if(diff>0)front++;//sum too small
+            else if(diff<0)back--;//sum too big
+            else return vector<int>{front+1,back+1};
+        
+        }
+        
+    }
+};
