acf12e5d8aa223b14c68e7f58e566bf8d38c105a
Haozhe Gu  (ASH) <ashghz26@gmail.com>
2017-07-16 16:29:16 -0700

    Update [102] [Binary Tree Level Order Traversal] [Medium] [Tree Breadth-first Search ]  [LinkedIn Facebook Amazon Microsoft Apple Bloomberg ]  [103 107 111 314 637].cpp

--- [102] [Binary Tree Level Order Traversal] [Medium] [Tree Breadth-first Search ]  [LinkedIn Facebook Amazon Microsoft Apple Bloomberg ]  [103 107 111 314 637].cpp
+++ [102] [Binary Tree Level Order Traversal] [Medium] [Tree Breadth-first Search ]  [LinkedIn Facebook Amazon Microsoft Apple Bloomberg ]  [103 107 111 314 637].cpp
@@ -31,3 +31,33 @@ public:
         if(root->right) recur(root->right,ans,depth+1);       
     }
 };
+
+
+/* Non Recursive, Use Queue and NULL to sentinal end of each level*/
+class Solution {
+public:
+    vector<vector<int> > levelOrder(TreeNode *root) {
+        vector<vector<int> >  result;
+        if (!root) return result;
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(NULL);
+        vector<int> cur_vec;
+        while(!q.empty()) {
+            TreeNode* t = q.front();
+            q.pop();
+            if (t==NULL) {
+                result.push_back(cur_vec);
+                cur_vec.resize(0);
+                if (q.size() > 0) {
+                    q.push(NULL);
+                }
+            } else {
+                cur_vec.push_back(t->val);
+                if (t->left) q.push(t->left);
+                if (t->right) q.push(t->right);
+            }
+        }
+        return result;
+    }
+};
