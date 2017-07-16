5d3a9f788c9b2ef4457c8e503eb00789ccd10300
Haozhe Gu  (ASH) <ashghz26@gmail.com>
2017-07-16 14:43:28 -0700

    Create [102] [Binary Tree Level Order Traversal] [Medium] [Tree Breadth-first Search ]  [LinkedIn Facebook Amazon Microsoft Apple Bloomberg ]  [103 107 111 314 637].cpp

---
+++ [102] [Binary Tree Level Order Traversal] [Medium] [Tree Breadth-first Search ]  [LinkedIn Facebook Amazon Microsoft Apple Bloomberg ]  [103 107 111 314 637].cpp
@@ -0,0 +1,33 @@
+/*
+Question# + Difficulty + Topic + Company + Similar_Question
+[102] [Binary Tree Level Order Traversal] [Medium] [Tree Breadth-first Search ] 
+[LinkedIn Facebook Amazon Microsoft Apple Bloomberg ] 
+[103 107 111 314 637].cpp
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        recur(root,ans,0);
+        return ans;
+    }
+    
+    void recur(TreeNode* root,vector<vector<int>> &ans,int depth) 
+    {
+        if(!root) return;
+        if(ans.size()<=depth) ans.push_back(vector<int>());
+        ans[depth].push_back(root->val);
+        if(root->left)  recur(root->left,ans,depth+1);
+        if(root->right) recur(root->right,ans,depth+1);       
+    }
+};
