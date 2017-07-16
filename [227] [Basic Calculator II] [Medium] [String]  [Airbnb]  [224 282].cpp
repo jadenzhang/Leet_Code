ad4354ddcbcc17c2ab2f3c2dc1c5ffedeebbb0c0
Haozhe Gu  (ASH) <ashghz26@gmail.com>
2017-07-15 21:20:03 -0700

    Create [227] [Basic Calculator II] [Medium] [String]  [Airbnb]  [224 282].cpp

---
+++ [227] [Basic Calculator II] [Medium] [String]  [Airbnb]  [224 282].cpp
@@ -0,0 +1,40 @@
+/*
+Question# + Difficulty + Topic + Company + Similar_Question
+[227] [Basic Calculator II] [Medium] [String] 
+[Airbnb] 
+[224 282].cpp
+*/
+
+
+class Solution {
+public:
+    int calculate(string s) {
+        int i = 0, ans = 0, num = 0;
+        int interim = 0;
+        char op = '+';
+        while(i < s.size()){
+            if(isdigit(s[i])){
+                num = 0;
+                while(i < s.size() && isdigit(s[i])){
+                    num = num * 10 + s[i] - '0';
+                    i++;
+                }
+                if(op == '+' || op == '-'){
+                    ans += interim;
+                    interim = num * (op == '-' ? -1 : 1);
+                }else if(op == '*'){
+                    interim *= num;
+                }else if(op == '/'){
+                    interim /= num;
+                }
+                continue;
+            }
+            else if(s[i] != ' ') op = s[i];
+           
+            i++;
+        }    
+
+        ans += interim;            
+        return ans;
+    }
+};
