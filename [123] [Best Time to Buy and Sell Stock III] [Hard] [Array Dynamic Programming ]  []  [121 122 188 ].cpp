04361ed1abb86a44e13e82ac09b05c17fc920279
Haozhe Gu  (ASH) <ashghz26@gmail.com>
2017-07-17 14:12:26 -0700

    Update [123] [Best Time to Buy and Sell Stock III] [Hard] [Array Dynamic Programming ]  []  [121 122 188 ].cpp

--- [123] [Best Time to Buy and Sell Stock III] [Hard] [Array Dynamic Programming ]  []  [121 122 188 ].cpp
+++ [123] [Best Time to Buy and Sell Stock III] [Hard] [Array Dynamic Programming ]  []  [121 122 188 ].cpp
@@ -35,3 +35,24 @@ public:
     }
 };
 
+
+/* Much Better O(N) Space O(1) Solution, inspired by the 121 problem */
+class Solution {
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int hold1 = INT_MIN, hold2 = INT_MIN;
+        int release1 = 0, release2 = 0;
+        for(int i=0; i<prices.size(); i++) { // Assume we only have 0 money at first
+
+            hold1    = max(hold1,    -prices[i]);          // The maximum if we've just buy  1st stock so far. 
+
+            release1 = max(release1, hold1+prices[i]);     // The maximum if we've just sold 1nd stock so far.
+
+            hold2    = max(hold2,    release1-prices[i]);  // The maximum if we've just buy  2nd stock so far.
+
+            release2 = max(release2, hold2+prices[i]);     // The maximum if we've just sold 2nd stock so far.
+        }
+        return release2; ///Since release1 is initiated as 0, so release2 will always higher than release1.
+    }
+};
