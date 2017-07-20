414b410da63c7178ca0273b7e4139e0856b14ea1
Haozhe Gu  (ASH) <ashghz26@gmail.com>
2017-07-19 22:26:58 -0700

    Update [105] [Construct Binary Tree from Preorder and Inorder Traversal] [Medium] [Tree Array Depth-first Search ]  [Bloomberg ]  [106].cpp

--- [105] [Construct Binary Tree from Preorder and Inorder Traversal] [Medium] [Tree Array Depth-first Search ]  [Bloomberg ]  [106].cpp
+++ [105] [Construct Binary Tree from Preorder and Inorder Traversal] [Medium] [Tree Array Depth-first Search ]  [Bloomberg ]  [106].cpp
@@ -64,3 +64,34 @@ public:
         return root;
     }
 };
+
+
+
+
+/* Recursion */
+TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
+    return create(preorder, inorder, 0, preorder.size() - 1, 0, inorder.size() - 1);
+}
+
+TreeNode* create(vector<int>& preorder, vector<int>& inorder, int ps, int pe, int is, int ie){
+    if(ps > pe){
+        return nullptr;
+    }
+    TreeNode* node = new TreeNode(preorder[ps]);
+    int pos;
+    for(int i = is; i <= ie; i++){
+        if(inorder[i] == node->val){
+            pos = i;
+            break;
+        }
+    }
+    node->left = create(preorder, inorder, ps + 1, ps + pos - is, is, pos - 1);
+    node->right = create(preorder, inorder, pe - ie + pos + 1, pe, pos + 1, ie);
+    return node;
+}
+
+/*
+The first element in preorder array can divide inorder array into two parts. Then we can divide preorder array into two parts.
+Make this element a node. And the left sub-tree of this node is the left part, right sub-tree of this node is the right part.
+This problem can be solved following this logic.
+*/
