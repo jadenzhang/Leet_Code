0782614ffe27cc189f79a3686857796c6876b770
Haozhe Gu  (ASH) <ashghz26@gmail.com>
2017-07-20 22:58:15 -0700

    Create [258] [Add Digits] [Medium] [Math]  [Adobe Microsoft ]  [202].cpp

---
+++ [258] [Add Digits] [Medium] [Math]  [Adobe Microsoft ]  [202].cpp
@@ -0,0 +1,34 @@
+/*
+Question# + Difficulty + Topic + Company + Similar_Question
+[258] [Add Digits] [Medium] [Math] 
+[Adobe Microsoft ] 
+[202].cpp
+*/
+
+/*
+For base b (decimal case b = 10), the digit root of an integer is:
+
+dr(n) = 0 if n == 0
+dr(n) = (b-1) if n != 0 and n % (b-1) == 0
+dr(n) = n mod (b-1) if n % (b-1) != 0
+or
+
+dr(n) = 1 + (n - 1) % 9
+Note here, when n = 0, since (n - 1) % 9 = -1, the return value is zero (correct).
+
+From the formula, we can find that the result of this problem is immanently periodic, with period (b-1).
+
+Output sequence for decimals (b = 10):
+
+~input: 0 1 2 3 4 ...
+output: 0 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 ....
+
+Henceforth, we can write the following code, whose time and space complexities are both O(1).
+/*
+
+class Solution {
+public:
+    int addDigits(int num) {
+        return 1 + (num - 1) % 9;
+    }
+};
