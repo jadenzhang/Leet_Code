eb04205f8274980896337ba9661f4caba71e98ea
Haozhe Gu  (ASH) <ashghz26@gmail.com>
2017-07-29 20:43:25 -0700

    Create [260] [Single Number III] [Medium] [Bit Manipulation ]  []  [136 137].cpp

---
+++ [260] [Single Number III] [Medium] [Bit Manipulation ]  []  [136 137].cpp
@@ -0,0 +1,53 @@
+/*
+Question# + Difficulty + Topic + Company + Similar_Question
+[260] [Single Number III] [Medium] [Bit Manipulation ] 
+[] 
+[136 137].cpp
+*/
+
+class Solution
+{
+public:
+    vector<int> singleNumber(vector<int>& nums) 
+    {
+        // Pass 1 : 
+        // Get the XOR of the two numbers we need to find
+        int diff = accumulate(nums.begin(), nums.end(), 0, bit_xor<int>());
+        // Get its last set bit
+        diff &= -diff;
+
+        // Pass 2 :
+        vector<int> rets = {0, 0}; // this vector stores the two numbers we will return
+        for (int num : nums)
+        {
+            if ((num & diff) == 0) // the bit is not set
+            {
+                rets[0] ^= num;
+            }
+            else // the bit is set
+            {
+                rets[1] ^= num;
+            }
+        }
+        return rets;
+    }
+};
+
+/*
+Once again, we need to use XOR to solve this problem. But this time, we need to do it in two passes:
+
+In the first pass, we XOR all elements in the array, and get the XOR of the two numbers we need to find. Note that since the two numbers are distinct, so there must be a set bit (that is, the bit with value '1') in the XOR result. Find
+out an arbitrary set bit (for example, the rightmost set bit).
+
+In the second pass, we divide all numbers into two groups, one with the aforementioned bit set, another with the aforementinoed bit unset. Two different numbers we need to find must fall into thte two distrinct groups. XOR numbers in each group, we can find a number in either group.
+
+Complexity:
+
+Time: O (n)
+
+Space: O (1)
+
+A Corner Case:
+
+When diff == numeric_limits<int>::min(), -diff is also numeric_limits<int>::min(). Therefore, the value of diff after executing diff &= -diff is still numeric_limits<int>::min(). The answer is still correct.
+*/
