6137069daaaf37c4ca7c63508b5ccebf7e28c9d3
Haozhe Gu  (ASH) <ashghz26@gmail.com>
2017-07-29 20:29:27 -0700

    Create [137] [Single Number II] [Medium] [Bit Manipulation]  []  [136 260].cpp

---
+++ [137] [Single Number II] [Medium] [Bit Manipulation]  []  [136 260].cpp
@@ -0,0 +1,38 @@
+/*
+Question# + Difficulty + Topic + Company + Similar_Question
+[137] [Single Number II] [Medium] [Bit Manipulation] 
+[] 
+[136 260].cpp
+*/
+
+/*
+The usual bit manipulation code is bit hard to get and replicate. 
+I like to think about the number in 32 bits and just count how many 1s are there in each bit,
+and sum %= 3 will clear it once it reaches 3. After running for all the numbers for each bit, if we have a 1,
+then that 1 belongs to the single number, we can simply move it back to its spot by doing ans |= sum << i;
+
+This has complexity of O(32n), which is essentially O(n) and very easy to think and implement. 
+Plus, you get a general solution for any times of occurrence. Say all the numbers have 5 times, just do sum %= 5.
+*/
+
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(int i =0;i<32;i++)
+        {
+            int sum = 0;
+            for(int j = 0;j<nums.size();j++)
+            {
+                if((nums[j]>>i)&1)
+                {
+                    sum++;
+                    sum %=3;
+                }
+            }
+            if(sum) ans|=sum<<i;
+        }
+        return ans;
+    }
+};
