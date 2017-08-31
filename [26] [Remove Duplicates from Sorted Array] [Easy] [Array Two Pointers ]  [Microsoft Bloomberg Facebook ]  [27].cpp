8d2b612cbbe66af5dde014921984538f40d586fd
Haozhe Gu  (ASH) <ashghz26@gmail.com>
2017-08-30 22:18:28 -0700

    Update [26] [Remove Duplicates from Sorted Array] [Easy] [Array Two Pointers ]  [Microsoft Bloomberg Facebook ]  [27].cpp

--- [26] [Remove Duplicates from Sorted Array] [Easy] [Array Two Pointers ]  [Microsoft Bloomberg Facebook ]  [27].cpp
+++ [26] [Remove Duplicates from Sorted Array] [Easy] [Array Two Pointers ]  [Microsoft Bloomberg Facebook ]  [27].cpp
@@ -9,16 +9,15 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         if(nums.size()==0) return 0;
-        int count = 1,dupli = nums[0];
-        for(int i =1,j=1;i<nums.size();i++)
+        int j=1,dupli = nums[0];
+        for(int i =1;i<nums.size();i++)
         {
             if(nums[i]!=dupli)//non-duplicate
             {
                 nums[j++] = nums[i];
                 dupli = nums[i];
-                count++;
             }
         }
-        return count;
+        return j;
     }
 };
