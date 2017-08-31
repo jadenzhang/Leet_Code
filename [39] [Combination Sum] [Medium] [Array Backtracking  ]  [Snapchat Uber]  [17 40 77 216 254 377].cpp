83cfd2ed6a902a82d90b82543bd25a3b2f66e08e
Haozhe Gu  (ASH) <ashghz26@gmail.com>
2017-08-31 03:07:37 -0700

    Create [39] [Combination Sum] [Medium] [Array Backtracking  ]  [Snapchat Uber]  [17 40 77 216 254 377].cpp

---
+++ [39] [Combination Sum] [Medium] [Array Backtracking  ]  [Snapchat Uber]  [17 40 77 216 254 377].cpp
@@ -0,0 +1,31 @@
+/*
+Question# + Difficulty + Topic + Company + Similar_Question
+[39] [Combination Sum] [Medium] [Array Backtracking  ] 
+[Snapchat Uber] 
+[17 40 77 216 254 377].cpp
+*/
+
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
+        sort(candidates.begin(),candidates.end());
+        recur(result,candidates,vector<int>(),target,0);
+        return result;
+    }
+    
+    void recur(vector<vector<int>> &result,vector<int> nums,vector<int> tmplist,int target,int pos)
+    {
+        if(target<0) return;//wrong combination, delete last
+        else if(target==0) result.push_back(tmplist);//finish one
+        else
+        {
+            for(int i =pos;i<nums.size();i++)
+            {
+                tmplist.push_back(nums[i]);
+                recur(result,nums,tmplist,target-nums[i],i);//so pos means can use more than once
+                tmplist.erase(tmplist.end()-1);// remove the num which cause negative result
+            }
+        }
+    }
+};
